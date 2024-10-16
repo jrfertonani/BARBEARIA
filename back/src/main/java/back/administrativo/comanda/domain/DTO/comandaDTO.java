b3d392ac39ddab67b4a7a631d437bbaf4dcf315b
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-16 19:49:51 -0300

    Comanda DTO - Entity

--- back/src/main/java/back/administrativo/comanda/domain/DTO/comandaDTO.java
+++ back/src/main/java/back/administrativo/comanda/domain/DTO/comandaDTO.java
@@ -3,13 +3,19 @@ package back.administrativo.comanda.domain.DTO;
 import lombok.Data;
 
 import java.io.Serial;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Data
 public class comandaDTO {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private Integer id;
+    private Long idComanda;
+    private Long idAgendamento;
+    private BigDecimal valorTotal;
+    private LocalDate dataPagamento;
+    private String formaPagamento;
 
 
 }
--- back/src/main/java/back/administrativo/comanda/domain/entity/Comanda.java
+++ back/src/main/java/back/administrativo/comanda/domain/entity/Comanda.java
@@ -1,13 +1,13 @@
 package back.administrativo.comanda.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import back.administrativo.agenda.domain.entity.Agenda;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Data       @Entity
 public class Comanda implements Serializable {
@@ -16,7 +16,16 @@ public class Comanda implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long idComanda;
+
+    private BigDecimal valorTotal;
+    private LocalDate dataPagamento;
+    private String formaPagamento; // Pode ser ENUM
+
+    // Relacionamento com Agenda (Cada comanda pertence a um único agendamento)
+    @OneToOne
+    @JoinColumn(name = "id_agendamento")
+    private Agenda agendamento;
 
 
 }
