a4b1cbebb9b8b11a56ce06803c3a5dc1995f0693
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-16 19:47:31 -0300

    Servico DTO - Entity

--- back/src/main/java/back/servicos/domain/entity/Servicos.java
+++ back/src/main/java/back/servicos/domain/entity/Servicos.java
@@ -1,13 +1,13 @@
 package back.servicos.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import back.administrativo.agenda.domain.entity.Agenda;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
 
 @Data       @Entity
 public class Servicos implements Serializable {
@@ -16,7 +16,13 @@ public class Servicos implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String nome;
-    private Double preco;
+    private Long idServico;
+
+    private String nomeServico;
+    private BigDecimal preco;
+    private String descricao;
+
+    // Relacionamento com Agenda (1 Serviço pode estar em vários agendamentos)
+    @OneToMany(mappedBy = "servico")
+    private List<Agenda> agendamentos;
 }
