8c63524859a229b4111216ca4a0c86f6f9c5594f
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-16 19:48:09 -0300

    Agenda service - repository

--- back/src/main/java/back/administrativo/agenda/repository/agendaRepository.java
+++ back/src/main/java/back/administrativo/agenda/repository/agendaRepository.java
@@ -4,6 +4,19 @@ import back.administrativo.agenda.domain.entity.Agenda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface agendaRepository extends JpaRepository<Agenda, Long> {
+/*
+    // Buscar agendamentos por cliente
+    List<Agenda> findByClienteId(Long idCliente);
+
+    // Buscar agendamentos por barbeiro
+    List<Agenda> findByBarbeiroId(Long idBarbeiro);
+
+    // Buscar agendamentos por serviço
+    List<Agenda> findByServicoId(Long idServico);
+
+ */
 }
