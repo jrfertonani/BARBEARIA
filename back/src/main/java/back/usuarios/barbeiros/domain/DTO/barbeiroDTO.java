b0f1bc12b7bddf9fec64cdb55d4c63e2cd1cc0c5
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-16 16:50:50 -0300

    Barbeiro DTO - Entity

--- back/src/main/java/back/usuarios/barbeiros/domain/DTO/barbeiroDTO.java
+++ back/src/main/java/back/usuarios/barbeiros/domain/DTO/barbeiroDTO.java
@@ -1,4 +1,20 @@
 package back.usuarios.barbeiros.domain.DTO;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.io.Serial;
+
+@Data
 public class barbeiroDTO {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+
+
 }
