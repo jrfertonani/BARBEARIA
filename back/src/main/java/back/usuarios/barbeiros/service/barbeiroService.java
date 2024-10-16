ee09c4c737f7feb2361f3e93a6207f9a26663da0
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-16 10:47:15 -0300

    components barbeiro

---
+++ back/src/main/java/back/usuarios/barbeiros/service/barbeiroService.java
@@ -0,0 +1,18 @@
+package back.usuarios.barbeiros.service;
+
+import back.usuarios.barbeiros.repository.barbeiroRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class barbeiroService {
+
+    @Autowired
+    public ModelMapper mapper;
+
+    @Autowired
+    public barbeiroRepository repository;
+
+
+}
