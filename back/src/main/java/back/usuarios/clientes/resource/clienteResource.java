076d1dd1001911243c8e04de7a9638b685bbd4ab
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-17 10:47:05 -0300

    cliente Entity

--- back/src/main/java/back/usuarios/clientes/resource/clienteResource.java
+++ back/src/main/java/back/usuarios/clientes/resource/clienteResource.java
@@ -1,12 +1,19 @@
 package back.usuarios.clientes.resource;
 
 
+import back.usuarios.barbeiros.domain.DTO.barbeiroDTO;
+import back.usuarios.barbeiros.domain.entity.Barbeiros;
+import back.usuarios.clientes.domain.DTO.clienteDTO;
+import back.usuarios.clientes.domain.entity.Clientes;
 import back.usuarios.clientes.service.clienteService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -21,6 +28,25 @@ public class clienteResource {
     public clienteService service;
 
 
+    @PostMapping
+    public ResponseEntity<clienteDTO> create(@RequestBody clienteDTO DTO){
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(
+                        service.create(DTO)
+                ).toUri();
+        return ResponseEntity.created(uri).body(DTO);
+    }
 
+    @GetMapping
+    public ResponseEntity<List<Clientes>> findAll(){
+        return ResponseEntity.ok().body(
+                service.findAll()
+                        .stream().map(x -> mapper.map(
+                                x, Clientes.class)
+                        ).toList()
+        );
+    }
 
 }
