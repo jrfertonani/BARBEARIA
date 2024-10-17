700a2c9fe71a7458c5aea24aa002d79d27a539e8
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-17 20:05:45 -0300

    findAll Agenda resource

--- back/src/main/java/back/administrativo/agenda/resource/agendaResource.java
+++ back/src/main/java/back/administrativo/agenda/resource/agendaResource.java
@@ -1,16 +1,22 @@
 package back.administrativo.agenda.resource;
 
 
+import back.administrativo.agenda.domain.entity.Agenda;
 import back.administrativo.agenda.service.agendaService;
+import back.administrativo.comanda.domain.entity.Comanda;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/servicos")
+@RequestMapping("/agenda")
 public class agendaResource {
 
 
@@ -19,6 +25,17 @@ public class agendaResource {
 
     @Autowired
     public agendaService service;
+
+
+    @GetMapping
+    public ResponseEntity<List<Agenda>> findAll(){
+        return ResponseEntity.ok().body(
+                service.findAll()
+                        .stream().map(x -> mapper.map(
+                                x, Agenda.class)
+                        ).toList()
+        );
+    }
 
 
 }
