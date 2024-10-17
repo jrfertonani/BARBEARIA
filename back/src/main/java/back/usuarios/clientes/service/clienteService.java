38bad133f92141de1e56481a928310fcbb2ffa64
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-17 10:49:03 -0300

    findById cliente resource

--- back/src/main/java/back/usuarios/clientes/service/clienteService.java
+++ back/src/main/java/back/usuarios/clientes/service/clienteService.java
@@ -1,9 +1,16 @@
 package back.usuarios.clientes.service;
 
+import back._config.Exeptions.serviceExceptions.ObjectNotFoundException;
+import back.usuarios.barbeiros.domain.DTO.barbeiroDTO;
+import back.usuarios.barbeiros.domain.entity.Barbeiros;
+import back.usuarios.clientes.domain.DTO.clienteDTO;
+import back.usuarios.clientes.domain.entity.Clientes;
 import back.usuarios.clientes.repository.clienteRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class clienteService {
@@ -13,6 +20,23 @@ public class clienteService {
 
     @Autowired
     public clienteRepository repository;
+
+    public Clientes create(clienteDTO DTO) {
+        return repository.save(
+                mapper.map(DTO, Clientes.class)
+        );
+    }
+
+    public List<Clientes> findAll() {
+        return repository.findAll();
+    }
+
+    public clienteDTO findById(Long id) {
+        return mapper.map(
+                repository.findById(id).orElseThrow(
+                        () -> new ObjectNotFoundException("Cliente não encontrado! ID: " +id)
+                ), clienteDTO.class);
+    }
 
 
 }
