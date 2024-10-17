5a3b44f93958db31e56d64a809fcd1f8b0d138ae
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-17 11:02:36 -0300

    create - findAll - Comanda service

--- back/src/main/java/back/administrativo/comanda/service/comandaService.java
+++ back/src/main/java/back/administrativo/comanda/service/comandaService.java
@@ -1,9 +1,15 @@
 package back.administrativo.comanda.service;
 
+import back.administrativo.comanda.domain.DTO.comandaDTO;
+import back.administrativo.comanda.domain.entity.Comanda;
 import back.administrativo.comanda.repository.comandaRepository;
+import back.servicos.domain.DTO.servicoDTO;
+import back.servicos.domain.entity.Servicos;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class comandaService {
@@ -14,6 +20,15 @@ public class comandaService {
     @Autowired
     public comandaRepository repository;
 
+    public Comanda create(comandaDTO DTO) {
+        return repository.save(
+                mapper.map(DTO, Comanda.class)
+        );
+    }
+
+    public List<Comanda> findAll() {
+        return repository.findAll();
+    }
 
 
 }
