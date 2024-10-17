49408a630d369c6d26c1d1409bdd286f4d1b9471
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-17 11:02:47 -0300

    create - findAll - Comanda resource

--- back/src/main/java/back/administrativo/comanda/resource/comandaResource.java
+++ back/src/main/java/back/administrativo/comanda/resource/comandaResource.java
@@ -1,12 +1,19 @@
 package back.administrativo.comanda.resource;
 
 
+import back.administrativo.comanda.domain.DTO.comandaDTO;
+import back.administrativo.comanda.domain.entity.Comanda;
 import back.administrativo.comanda.service.comandaService;
+import back.servicos.domain.DTO.servicoDTO;
+import back.servicos.domain.entity.Servicos;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -20,5 +27,25 @@ public class comandaResource {
     @Autowired
     public comandaService service;
 
+    @PostMapping
+    public ResponseEntity<comandaDTO> create(@RequestBody comandaDTO DTO){
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(
+                        service.create(DTO)
+                ).toUri();
+        return ResponseEntity.created(uri).body(DTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Comanda>> findAll(){
+        return ResponseEntity.ok().body(
+                service.findAll()
+                        .stream().map(x -> mapper.map(
+                                x, Comanda.class)
+                        ).toList()
+        );
+    }
 
 }
