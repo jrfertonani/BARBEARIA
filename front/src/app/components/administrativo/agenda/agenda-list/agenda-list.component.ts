cce7353c1766d828d25b05719900a7c06ee3b2eb
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-18 11:05:12 -0300

    components administrativo comanda - list

---
+++ front/src/app/components/administrativo/agenda/agenda-list/agenda-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agenda-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './agenda-list.component.html',
+  styleUrl: './agenda-list.component.css'
+})
+export class AgendaListComponent {
+
+}
