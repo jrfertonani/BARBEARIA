9b6bd393bcef4bab224e6f6418aa538304442578
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-18 11:00:15 -0300

    components service-create

---
+++ front/src/app/components/servicos/servico-creat/servico-creat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servico-creat',
+  standalone: true,
+  imports: [],
+  templateUrl: './servico-creat.component.html',
+  styleUrl: './servico-creat.component.css'
+})
+export class ServicoCreatComponent {
+
+}
