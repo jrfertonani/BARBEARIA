34a6fcff8a30bf721142fa53e9d04d24c8080c35
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-18 14:42:21 -0300

    list - servicos

--- front/src/app/components/servicos/servico-list/servico-list.component.ts
+++ front/src/app/components/servicos/servico-list/servico-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServicosService } from '../../../services/servicos/servicos.service';
+import { Servicos } from '../../../models/servicos/Servicos';
 
 @Component({
   selector: 'app-servico-list',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './servico-list.component.html',
   styleUrl: './servico-list.component.css'
 })
-export class ServicoListComponent {
+export class ServicoListComponent implements OnInit{
+
+  servico: Servicos[] = [];
+  servicoGeral: Servicos[] = [];
+
+constructor(private serviceServico: ServicosService) {}
+
+
+ngOnInit(): void {
+   this.serviceServico.List().subscribe((servico) =>{
+    this.servico = servico;
+    this.servicoGeral = servico;
+    console.log(servico);
+   })
+
+}
 
 }
--- front/src/app/services/servicos/servicos.service.ts
+++ front/src/app/services/servicos/servicos.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Servicos } from '../../models/servicos/Servicos';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +12,9 @@ export class ServicosService {
   URL = 'http://localhost:8080/servicos';
 
   constructor(private http: HttpClient) {}
+
+
+  List():Observable<Servicos[]>{
+    return this.http.get<Servicos[]>(this.URL);
+  }
 }
