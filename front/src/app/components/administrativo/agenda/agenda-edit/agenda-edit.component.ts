542b445eae0da1df69354f818d240d40f8336682
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-18 11:08:22 -0300

    components administrativo comanda - edit

---
+++ front/src/app/components/administrativo/agenda/agenda-edit/agenda-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agenda-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './agenda-edit.component.html',
+  styleUrl: './agenda-edit.component.css'
+})
+export class AgendaEditComponent {
+
+}
