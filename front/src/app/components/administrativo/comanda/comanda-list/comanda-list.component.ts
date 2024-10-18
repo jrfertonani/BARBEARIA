0f8da93dd75cd901a0ef7ca6d8adcbdb0cacc2bc
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-18 14:49:38 -0300

    list - comandas

--- front/src/app/components/administrativo/comanda/comanda-list/comanda-list.component.ts
+++ front/src/app/components/administrativo/comanda/comanda-list/comanda-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Comanda } from '../../../../models/administrativoModels/Comanda';
+import { ComandasService } from '../../../../services/administrativo/comandas/comandas.service';
 
 @Component({
   selector: 'app-comanda-list',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './comanda-list.component.html',
   styleUrl: './comanda-list.component.css'
 })
-export class ComandaListComponent {
+export class ComandaListComponent implements OnInit{
+
+  comanda: Comanda[] = [];
+  comandaGeral: Comanda [] = [];
+
+  constructor(private serviceComandas: ComandasService){}
+
+
+
+
+  ngOnInit(): void {
+    this.serviceComandas.List().subscribe((comanda) => {
+      this.comanda = comanda;
+      this.comandaGeral = comanda;
+      console.log(comanda);
+    })
+  }
 
 }
--- front/src/app/services/administrativo/comandas/comandas.service.ts
+++ front/src/app/services/administrativo/comandas/comandas.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comanda } from '../../../models/administrativoModels/Comanda';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,9 @@ export class ComandasService {
   URL = 'http://localhost:8080/comandas';
 
   constructor(private http: HttpClient) {}
+
+  List():Observable<Comanda[]>{
+    return this.http.get<Comanda[]>(this.URL);
+  }
 
 }
