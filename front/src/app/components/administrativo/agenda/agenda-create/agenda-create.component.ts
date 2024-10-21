8a03bc916422736dd1aa0f4c7d1e37526fbf2f08
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-21 15:08:00 -0300

    create agenda create

--- front/src/app/components/administrativo/agenda/agenda-create/agenda-create.component.ts
+++ front/src/app/components/administrativo/agenda/agenda-create/agenda-create.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { AgendaFormComponent } from "../agenda-form/agenda-form.component";
+import { AgendaService } from '../../../../services/administrativo/agenda/agenda.service';
+import { Router } from '@angular/router';
+import { Agenda } from '../../../../models/administrativoModels/Agenda';
 
 @Component({
   selector: 'app-agenda-create',
   standalone: true,
-  imports: [],
+  imports: [AgendaFormComponent],
   templateUrl: './agenda-create.component.html',
   styleUrl: './agenda-create.component.css'
 })
 export class AgendaCreateComponent {
 
+
+  constructor(private serviceAgenda: AgendaService, private router: Router){}
+
+
+  createAgenda(agenda: Agenda){
+    this.serviceAgenda.createAgenda(agenda).subscribe(agenda => {
+      this.router.navigate(['/agenda'])
+      console.log(agenda)
+    })
+    console.log(agenda)
+  }
+
+
+
 }
+
