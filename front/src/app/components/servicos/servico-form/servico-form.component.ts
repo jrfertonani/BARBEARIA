3efcc83027bf72760947db90e1a515e204ee7b08
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-21 14:03:21 -0300

    create service front

--- front/src/app/components/servicos/servico-form/servico-form.component.ts
+++ front/src/app/components/servicos/servico-form/servico-form.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {  Component, EventEmitter, Input, OnInit, Output  } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { Servicos } from '../../../models/servicos/Servicos';
 
 @Component({
   selector: 'app-servico-form',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './servico-form.component.html',
   styleUrl: './servico-form.component.css'
 })
-export class ServicoFormComponent {
+export class ServicoFormComponent implements OnInit{
+
+  @Output () onSubmit = new EventEmitter<Servicos>();
+
+  constructor( private router: Router){}
+
+
+  servicoForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.servicoForm = new FormGroup({
+      id: new FormControl(0),
+      nomeServico: new FormControl(''),
+      preco: new FormControl(0),
+      descricao: new FormControl('')
+    });
+  }
+
+  submit(){
+    this.onSubmit.emit(this.servicoForm.value);
+
+    console.log(this.servicoForm.value);
+  }
 
 }
