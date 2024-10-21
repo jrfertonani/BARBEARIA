b6cdcbc51afe33d0d20349b5a4389f3b543b4c3c
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-21 11:34:10 -0300

    create clientes-create

--- front/src/app/components/usuarios/clientes/cliente-create/cliente-create.component.ts
+++ front/src/app/components/usuarios/clientes/cliente-create/cliente-create.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ClienteFormComponent } from "../cliente-form/cliente-form.component";
+import { Clientes } from '../../../../models/usuarios/Clientes';
+import { ClientesService } from '../../../../services/usuarios/clientes/clientes.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cliente-create',
   standalone: true,
-  imports: [],
+  imports: [ClienteFormComponent],
   templateUrl: './cliente-create.component.html',
   styleUrl: './cliente-create.component.css'
 })
 export class ClienteCreateComponent {
+
+  constructor(private serviceCliente: ClientesService, private router: Router){}
+
+  createCliente(clientes : Clientes){
+    this.serviceCliente.createCliente(clientes).subscribe(cliente => {
+      this.router.navigate(['/clientes']);
+      console.log(cliente);
+    })
+  }
 
 }
