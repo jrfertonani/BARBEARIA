8df91f456aadd7a70227f7f4183a8fa0f5154ee3
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-21 15:08:15 -0300

    create agenda form

--- front/src/app/components/administrativo/agenda/agenda-form/agenda-form.component.ts
+++ front/src/app/components/administrativo/agenda/agenda-form/agenda-form.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {  Component, EventEmitter, Input, OnInit, Output  } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { Agenda } from '../../../../models/administrativoModels/Agenda';
 
 @Component({
   selector: 'app-agenda-form',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './agenda-form.component.html',
   styleUrl: './agenda-form.component.css'
 })
-export class AgendaFormComponent {
+export class AgendaFormComponent implements OnInit{
+
+  @Output () onSubmit = new EventEmitter<Agenda>();
+
+  constructor( private router: Router){}
+
+  agendaForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.agendaForm = new FormGroup({
+      id: new FormControl(0),
+      dataHora: new FormControl(0),
+      cliente: new FormControl(''),
+      barbeiro: new FormControl(''),
+      servico: new FormControl('')
+    })
+
+  }
+
+
+  submit(){
+    this.onSubmit.emit(this.agendaForm.value)
+
+   // console.log(this.agendaForm.value)//Testar o botao cadastrar
+  }
 
 }
+
