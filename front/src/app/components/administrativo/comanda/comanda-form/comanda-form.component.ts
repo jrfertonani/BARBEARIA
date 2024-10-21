869cff9d6fbf6f95655f968bd3bf9ac6c7e492a9
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-21 14:31:55 -0300

    create comanda form

--- front/src/app/components/administrativo/comanda/comanda-form/comanda-form.component.ts
+++ front/src/app/components/administrativo/comanda/comanda-form/comanda-form.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import {  Component, EventEmitter, Input, OnInit, Output  } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { Comanda } from '../../../../models/administrativoModels/Comanda';
+
 
 @Component({
   selector: 'app-comanda-form',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './comanda-form.component.html',
   styleUrl: './comanda-form.component.css'
 })
-export class ComandaFormComponent {
+export class ComandaFormComponent implements OnInit{
 
+  @Output () onSubmit = new EventEmitter<Comanda>();
+
+  comandaForm!: FormGroup;
+
+
+  constructor( private router: Router){}
+
+
+  ngOnInit(): void {
+    this.comandaForm = new FormGroup({
+      id: new FormControl(0),
+      valorTotal: new FormControl(0),
+      dataPagamento: new FormControl(''),
+      formaPagamento: new FormControl(''),
+      agendamento: new FormControl('')
+    })
+  }
+
+
+  submit(){
+    this.onSubmit.emit(this.comandaForm.value);
+    console.log(this.comandaForm.value)
+  }
 }
