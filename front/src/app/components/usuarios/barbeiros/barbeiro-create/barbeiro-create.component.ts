674921ff81fb901e49dbc6573bc275402d57b903
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-21 13:20:32 -0300

    create barbeiro

--- front/src/app/components/usuarios/barbeiros/barbeiro-create/barbeiro-create.component.ts
+++ front/src/app/components/usuarios/barbeiros/barbeiro-create/barbeiro-create.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { BarbeiroFormComponent } from "../barbeiro-form/barbeiro-form.component";
+import { BarbeirosService } from '../../../../services/usuarios/barbeiros/barbeiros.service';
+import { Router } from '@angular/router';
+import { Barbeiros } from '../../../../models/usuarios/Barbeiros';
 
 @Component({
   selector: 'app-barbeiro-create',
   standalone: true,
-  imports: [],
+  imports: [BarbeiroFormComponent],
   templateUrl: './barbeiro-create.component.html',
   styleUrl: './barbeiro-create.component.css'
 })
 export class BarbeiroCreateComponent {
+
+  constructor(private serviceBarbeiro: BarbeirosService, private router: Router){}
+
+  criateBrbeiro(barbeiro: Barbeiros){
+    this.serviceBarbeiro.crearBarbeiro(barbeiro).subscribe(barbeiro =>{
+      this.router.navigate(['/barbeiros'])
+      console.log(barbeiro);
+    })
+
+  }
 
 }
