f3ebde002f7ac59b0b1d0813289ff8de15c46f34
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-20 22:49:50 -0300

    implementando HTML - clienteForm

--- front/src/app/components/usuarios/clientes/cliente-form/cliente-form.component.ts
+++ front/src/app/components/usuarios/clientes/cliente-form/cliente-form.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cliente-form',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './cliente-form.component.html',
   styleUrl: './cliente-form.component.css'
 })
-export class ClienteFormComponent {
+export class ClienteFormComponent implements OnInit{
+
+
+  constructor( private router: Router){}
+
+
+  clienteForm!: FormGroup;
+
+  ngOnInit(): void {
+
+  }
+
+
 
 }
