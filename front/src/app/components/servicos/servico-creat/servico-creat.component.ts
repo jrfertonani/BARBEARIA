8033b0dc51e98bc33c3f43b4302d845329c2ff3a
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-21 14:03:05 -0300

    create service

--- front/src/app/components/servicos/servico-creat/servico-creat.component.ts
+++ front/src/app/components/servicos/servico-creat/servico-creat.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServicoFormComponent } from "../servico-form/servico-form.component";
+import { ServicosService } from '../../../services/servicos/servicos.service';
+import { Router } from '@angular/router';
+import { Servicos } from '../../../models/servicos/Servicos';
 
 @Component({
   selector: 'app-servico-creat',
   standalone: true,
-  imports: [],
+  imports: [ServicoFormComponent],
   templateUrl: './servico-creat.component.html',
   styleUrl: './servico-creat.component.css'
 })
 export class ServicoCreatComponent {
+
+
+  constructor(private serviceServico: ServicosService,  private router: Router){}
+
+
+  crearServico(servico: Servicos){
+    this.serviceServico.createSErvico(servico).subscribe(servico => {
+      this.router.navigate(['/servicos']);
+
+      console.log(servico);
+    })
+  }
 
 }
