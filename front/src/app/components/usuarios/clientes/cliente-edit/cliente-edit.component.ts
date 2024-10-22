2df024e69b34b5db5f0cd7ec3f4cff6367880866
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-21 22:18:57 -0300

    cliente edit

--- front/src/app/components/usuarios/clientes/cliente-edit/cliente-edit.component.ts
+++ front/src/app/components/usuarios/clientes/cliente-edit/cliente-edit.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClienteFormComponent } from "../cliente-form/cliente-form.component";
+import { ClientesService } from '../../../../services/usuarios/clientes/clientes.service';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Clientes } from '../../../../models/usuarios/Clientes';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cliente-edit',
   standalone: true,
-  imports: [],
+  imports: [ClienteFormComponent, CommonModule],
   templateUrl: './cliente-edit.component.html',
   styleUrl: './cliente-edit.component.css'
 })
-export class ClienteEditComponent {
+export class ClienteEditComponent implements OnInit{
 
+  clientes!: Clientes;
+
+  btnAcao = "Editar"
+  descTitulo = "Editar Cliente"
+
+  constructor(private serviceCliente: ClientesService, private router: Router, private route: ActivatedRoute){}
+
+
+  ngOnInit(): void {
+
+   // console.log(1) // busca o id
+
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.serviceCliente.findById(id).subscribe(clientes => {
+
+    //  console.log(2);  //retorna o id
+
+      this.clientes = clientes;
+    });
+
+  }
+
+
+  editCliente(clientes: Clientes){
+    this.serviceCliente.editCliente(clientes).subscribe(clientes => {
+      this.router.navigate(['/clientes'])
+      console.log(clientes);
+    })
+
+  }
 }
+
