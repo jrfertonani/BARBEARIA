c310f38626383ca22426079ff1b1afc9f6402c0a
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-22 13:07:06 -0300

    ts form

--- front/src/app/components/usuarios/barbeiros/barbeiro-form/barbeiro-form.component.ts
+++ front/src/app/components/usuarios/barbeiros/barbeiro-form/barbeiro-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { Barbeiros } from '../../../../models/usuarios/Barbeiros';
@@ -14,17 +14,26 @@ export class BarbeiroFormComponent implements OnInit{
 
   @Output () onSubmit = new EventEmitter<Barbeiros>();
 
+  @Input () dadosBarbeiro : Barbeiros | null = null;
+
+  @Input() btnAcao! : string;
+  @Input() descTitulo!: string;
+
   constructor( private router: Router){}
 
 
   barbeiroForm!: FormGroup;
 
   ngOnInit(): void {
+
+    console.log(this.dadosBarbeiro); // checar se esta retornando
+    //console.log(3) //teste para ver qual requisição esta chegando primeiro -- NÂO ESQUECER DO *ngIf no templete html de editar
+
     this.barbeiroForm = new FormGroup({
-      id: new FormControl(0),
-      nome: new FormControl(''),
-      telefone: new FormControl(0),
-      especialidade: new FormControl('')
+      id: new FormControl(this.dadosBarbeiro ? this.dadosBarbeiro.idBarbeiro : 0),
+      nome: new FormControl(this.dadosBarbeiro ? this.dadosBarbeiro.nome :''),
+      telefone: new FormControl(this.dadosBarbeiro ? this.dadosBarbeiro.telefone : 0),
+      especialidade: new FormControl(this.dadosBarbeiro ? this.dadosBarbeiro.especialidade :'')
     })
 
   }
@@ -33,7 +42,8 @@ export class BarbeiroFormComponent implements OnInit{
 
   submit(){
     this.onSubmit.emit(this.barbeiroForm.value);
-    console.log(this.barbeiroForm.value)
+    //console.log(this.barbeiroForm.value)
   }
 
 }
+
