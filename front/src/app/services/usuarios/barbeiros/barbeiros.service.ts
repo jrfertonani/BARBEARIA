6c433d0dd9a39e4cc4350a1be61ab9d174137b22
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-22 19:19:52 -0300

    barbeiros service

--- front/src/app/services/usuarios/barbeiros/barbeiros.service.ts
+++ front/src/app/services/usuarios/barbeiros/barbeiros.service.ts
@@ -31,7 +31,8 @@ export class BarbeirosService {
   }
 
   editBarbeiro(barbeiro: Barbeiros):Observable<Barbeiros>{
-    return this.http.put<Barbeiros>(this.URL,barbeiro)
+    const url = `${this.URL}/${barbeiro.idBarbeiro}`
+    return this.http.put<Barbeiros>(url,barbeiro)
   }
 
 }
