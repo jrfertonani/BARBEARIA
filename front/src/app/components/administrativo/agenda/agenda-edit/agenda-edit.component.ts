9dfa8b93e2ef113d57867d9bfa14cb535aa6274c
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-23 16:04:46 -0300

    ts update servico edit

--- front/src/app/components/administrativo/agenda/agenda-edit/agenda-edit.component.ts
+++ front/src/app/components/administrativo/agenda/agenda-edit/agenda-edit.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AgendaFormComponent } from "../agenda-form/agenda-form.component";
+import { ActivatedRoute, Router } from '@angular/router';
+import { Agenda } from '../../../../models/administrativoModels/Agenda';
+import { AgendaService } from '../../../../services/administrativo/agenda/agenda.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-agenda-edit',
   standalone: true,
-  imports: [],
+  imports: [AgendaFormComponent, CommonModule],
   templateUrl: './agenda-edit.component.html',
   styleUrl: './agenda-edit.component.css'
 })
-export class AgendaEditComponent {
+export class AgendaEditComponent implements OnInit{
+
+  agenda!: Agenda;
+
+  btnAcao = "Editar";
+  descTitulo = "Editar agenda";
+
+  constructor(private serviceAgenda: AgendaService, private router: Router, private route: ActivatedRoute){}
+
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+
+    this.serviceAgenda.findById(id).subscribe(agenda =>{
+      this.agenda = agenda;
+    })
+
+  }
+
+  editAgenda(agenda : Agenda){
+    this.serviceAgenda.editAgenda(agenda).subscribe(agenda => {
+      this.router.navigate(['/agenda'])
+    })
+  }
+
+
 
 }
