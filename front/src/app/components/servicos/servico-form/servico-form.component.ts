fe1bc767ee171e9dc0d212bd4f702892e8fca115
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-23 11:46:29 -0300

    ts form servico

--- front/src/app/components/servicos/servico-form/servico-form.component.ts
+++ front/src/app/components/servicos/servico-form/servico-form.component.ts
@@ -14,24 +14,34 @@ export class ServicoFormComponent implements OnInit{
 
   @Output () onSubmit = new EventEmitter<Servicos>();
 
+  @Input() dadosServico : Servicos | null = null;
+
+
+
+  @Input() btnAcao! : string;
+  @Input() descTitulo! : string;
+
   constructor( private router: Router){}
 
 
   servicoForm!: FormGroup;
 
   ngOnInit(): void {
+
+   // console.log(3)
+
     this.servicoForm = new FormGroup({
-      id: new FormControl(0),
-      nomeServico: new FormControl(''),
-      preco: new FormControl(0),
-      descricao: new FormControl('')
+      idServico: new FormControl(this.dadosServico ? this.dadosServico.idServico : 0),
+      nomeServico: new FormControl(this.dadosServico ? this.dadosServico.nomeServico : ''),
+      preco: new FormControl(this.dadosServico ? this.dadosServico.preco : 0),
+      descricao: new FormControl(this.dadosServico ? this.dadosServico.descricao : '')
     });
   }
 
   submit(){
     this.onSubmit.emit(this.servicoForm.value);
 
-    console.log(this.servicoForm.value);
+    //console.log(this.servicoForm.value);
   }
 
 }
