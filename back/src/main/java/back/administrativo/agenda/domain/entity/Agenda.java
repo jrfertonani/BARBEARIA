fb9e42785a92ec2b449c08dc17fd26d56ef7127b
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-27 12:38:02 -0300

    Agenda entity

--- back/src/main/java/back/administrativo/agenda/domain/entity/Agenda.java
+++ back/src/main/java/back/administrativo/agenda/domain/entity/Agenda.java
@@ -4,14 +4,15 @@ import back.administrativo.comanda.domain.entity.Comanda;
 import back.servicos.domain.entity.Servicos;
 import back.usuarios.barbeiros.domain.entity.Barbeiros;
 import back.usuarios.clientes.domain.entity.Clientes;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-@Data       @Entity
+@Entity
 public class Agenda implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
@@ -20,6 +21,7 @@ public class Agenda implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idAgendamento;
 
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime dataHora;
 
     // Relacionamento com Cliente (Muitos agendamentos para um cliente)
@@ -42,4 +44,76 @@ public class Agenda implements Serializable {
     private Comanda comanda;
 
 
+    public Agenda(Long idAgendamento, LocalDateTime dataHora, Clientes cliente, Barbeiros barbeiro, Servicos servico, Comanda comanda) {
+        this.idAgendamento = idAgendamento;
+        this.dataHora = dataHora;
+        this.cliente = cliente;
+        this.barbeiro = barbeiro;
+        this.servico = servico;
+        this.comanda = comanda;
+    }
+
+    public Agenda() {
+    }
+
+    public Long getIdAgendamento() {
+        return idAgendamento;
+    }
+
+    public void setIdAgendamento(Long idAgendamento) {
+        this.idAgendamento = idAgendamento;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public Clientes getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Clientes cliente) {
+        this.cliente = cliente;
+    }
+
+    public Barbeiros getBarbeiro() {
+        return barbeiro;
+    }
+
+    public void setBarbeiro(Barbeiros barbeiro) {
+        this.barbeiro = barbeiro;
+    }
+
+    public Servicos getServico() {
+        return servico;
+    }
+
+    public void setServico(Servicos servico) {
+        this.servico = servico;
+    }
+
+    public Comanda getComanda() {
+        return comanda;
+    }
+
+    public void setComanda(Comanda comanda) {
+        this.comanda = comanda;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Agenda agenda = (Agenda) o;
+        return Objects.equals(idAgendamento, agenda.idAgendamento);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(idAgendamento);
+    }
 }
