deab1fd35baeb6f2dbf8c7540e37970113a5dd6d
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-11-17 14:09:25 -0300

    atualizacoes agenda DTO

--- back/src/main/java/back/administrativo/agenda/domain/DTO/agendaDTO.java
+++ back/src/main/java/back/administrativo/agenda/domain/DTO/agendaDTO.java
@@ -13,13 +13,58 @@ public class agendaDTO {
     private static final long serialVersionUID = 1L;
 
     private Long idAgendamento;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime dataHora;
     private Long idCliente;
     private Long idBarbeiro;
     private Long idServico;
-
-
-    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-    private LocalDateTime dataHora;
-
     private Long idComanda;
+
+    public Long getIdAgendamento() {
+        return idAgendamento;
+    }
+
+    public void setIdAgendamento(Long idAgendamento) {
+        this.idAgendamento = idAgendamento;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Long getIdBarbeiro() {
+        return idBarbeiro;
+    }
+
+    public void setIdBarbeiro(Long idBarbeiro) {
+        this.idBarbeiro = idBarbeiro;
+    }
+
+    public Long getIdServico() {
+        return idServico;
+    }
+
+    public void setIdServico(Long idServico) {
+        this.idServico = idServico;
+    }
+
+    public Long getIdComanda() {
+        return idComanda;
+    }
+
+    public void setIdComanda(Long idComanda) {
+        this.idComanda = idComanda;
+    }
 }
